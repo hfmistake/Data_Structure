0f1896a3b8b3e49c2300dcc82dfa779ca6e2c159
Héberty "Mistake" Filho <hfmistake@gmail.com>
2023-10-08 05:06:50 -0300

    Organizando melhor os arquivos

--- src/AtvEmSala01/Main.java
+++ src/AtvEmSala01/Main.java
@@ -2,7 +2,33 @@ package AtvEmSala01;
 
 import java.util.Scanner;
 
-public class ControlaAgenda {
+class Contato {
+    private String nome;
+    private String telefone;
+
+    public Contato(String nome, String telefone) {
+        this.setNome(nome);
+        this.setTelefone(telefone);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
+
+class ControlaAgenda {
     Scanner input = new Scanner(System.in);
     private final Contato[] agenda;
 
@@ -137,6 +163,31 @@ public class ControlaAgenda {
         }
         if (!existe) {
             System.out.println("Agenda não possui contatos que iniciam com a letra informada!");
+        }
+    }
+}
+
+public class Main {
+        public static void main(String[] args) {
+        ControlaAgenda agenda = new ControlaAgenda(50);
+        Scanner input = new Scanner(System.in);
+        int opcao = -1;
+        agenda.mostrarMenu();
+        while (opcao != 0) {
+            System.out.print("Para exibir o menu novamente insira [8]\nInsira a opção desejada: ");
+            opcao = Integer.parseInt(input.nextLine());
+            switch (opcao) {
+                case 1 -> agenda.adicionarContato(agenda.GerarContatoPrompt());
+                case 2 -> agenda.adicionarContatoPos(agenda.PosicaoPrompt(), agenda.GerarContatoPrompt());
+                case 3 -> agenda.excluirContato(agenda.NomePrompt());
+                case 4 -> agenda.quantidadeContatos();
+                case 5 -> agenda.buscarContato(agenda.NomePrompt());
+                case 6 -> agenda.imprimirTodosContatos();
+                case 7 -> agenda.imprimirContatos(agenda.LetraPrompt());
+                case 8 -> agenda.mostrarMenu();
+                case 0 -> System.out.println("Saindo...");
+                default -> System.out.println("Opção inválida!");
+            }
         }
     }
 }
